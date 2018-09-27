6c963789635419b34a0a595b1d0bd173df80d792
outlawstar4761 <outlawstar4761@gmail.com>
2018-09-27 16:14:45 -0500

    Add files via upload

--- startDaemon.php
+++ startDaemon.php
@@ -1,9 +1,19 @@
 <?php
 
+$params = $argv;
+
+if(!isset($params[1]) || !isset($params[2])){
+    echo "USAGE:\n";
+    echo "php startDaemon.php {HOSTNAME_OR_IP_1} {HOSTNAME_OR_IP_2}";
+    exit;
+}else{
+    $host1 = $params[1];
+    $host2 = $params[2];
+}
 while(true){
     $number = mt_rand(1,10);
     $option = $number % 2 == 0 ? 1 : 0;
-    $cmd = './run.sh ' . $option;
+    $cmd = './run.sh ' . $host1 . ' ' . $host2 . ' ' . $option;
     exec($cmd);
     sleep(120);
 }
